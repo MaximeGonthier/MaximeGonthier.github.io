c86de5809f7152e1975f1b88bfa67a23bbcb4586
laurelinemartin <laureline.martin@hotmail.com>
2025-02-06 00:32:10 +0100

    Mentoring section + UI

--- script.js
+++ script.js
@@ -9,4 +9,38 @@ document.addEventListener('DOMContentLoaded', function () {
       mainMenu.style.display = 'flex';
     }
   });
+
 });
+
+
+fetch("data.json")
+.then( res => { 
+  return res.json(); })
+  .then( data => {
+    show(data);})
+    
+    
+function show(data){
+  var mentoringTable = document.getElementById("mentoring-table");
+  var thead = document.createElement("thead");
+      thead.className = "bold-table"
+  var tr = document.createElement("tr");
+    Object.keys(data.mentoring[0]).forEach(key => {
+      var th = document.createElement("th");
+      th.innerHTML = key;
+      tr.appendChild(th);
+    });
+    thead.appendChild(tr)
+  data.mentoring.forEach(elements => {
+    var tr = document.createElement("tr");
+    tr.innerHTML = "";
+    Object.values(elements).forEach(value => {
+      var td = document.createElement("td");
+      td.innerHTML = value;
+      tr.appendChild(td); 
+    });
+    mentoringTable.appendChild(thead);
+    mentoringTable.appendChild(tr);
+  });
+}
+
